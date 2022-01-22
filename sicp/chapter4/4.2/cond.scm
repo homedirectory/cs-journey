2bc4e8b5897ff322a1277af3f8782e846663937c
homedirectory <dalvkylib@gmail.com>
2022-01-22 19:37:19 +0200

    modified cond.scm to correctly craft "cond" expressions manually

--- sicp/chapter4/4.2/cond.scm
+++ sicp/chapter4/4.2/cond.scm
@@ -6,25 +6,38 @@
 ; cond can be written in terms of nested if else clauses.
 ; such expressions are called derived expressions.
 (define (cond? exp) (tagged-list? exp 'cond))
-(define (cond-clauses exp) (cdr exp))
+(define (cond-clauses exp) (cadr exp))
 (define (cond-else-clause? clause)
   (eq? (cond-predicate clause) 'else))
 (define (cond-predicate clause) (car clause))
-(define (cond-actions clause) (cdr clause))
+(define (cond-actions clause) (cadr clause))
 (define (cond->if exp) (expand-clauses (cond-clauses exp)))
+(define (sequence? exp) (not (or (self-evaluating? exp) (variable? exp) (quoted? exp))))
 (define (expand-clauses clauses)
   (if (null? clauses)
       'false      ; no else clause
       (let ((first (car clauses))
             (rest (cdr clauses)))
-        (if (cond-else-clause? first)
-            (if (null? rest)
-                (sequence->exp (cond-actions first))
-                (error "ELSE clause isn't last: COND->IF"
-                       clauses))
-            (make-if (cond-predicate first)
-                     (sequence->exp (cond-actions first))
-                     (expand-clauses rest))))))
+        (let ((actions (cond-actions first)))
+          (if (cond-else-clause? first)
+              (if (null? rest)
+                  (if (sequence? actions)
+                      (sequence->exp actions)
+                      actions)
+                  (error "ELSE clause isn't last: COND->IF"
+                         clauses))
+              (if (sequence? actions)
+                  (make-if (cond-predicate first)
+                           (sequence->exp actions)
+                           (expand-clauses rest))
+                  (make-if (cond-predicate first)
+                           actions
+                           (expand-clauses rest))))))))
+
+(define (make-cond clauses)
+  (list 'cond clauses))
+(define (make-cond-clause predicate actions)
+  (list predicate actions))
 
 
 ;---------------------------------------------------------------
